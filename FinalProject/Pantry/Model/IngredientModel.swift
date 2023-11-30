b1db982fe6514d4a65254dda4e9100249f562bbf
tehya-laughlin <tehyanlaugh.shop@gmail.com>
2023-11-30 14:53:31 -0700

    removed commented sections

--- FinalProject/Pantry/Model/IngredientModel.swift
+++ FinalProject/Pantry/Model/IngredientModel.swift
@@ -27,10 +27,6 @@ struct IngredientQueryBody: Codable {
 
 class IngredientModel: Codable, Identifiable {
     
-   /* enum CodingKeys: CodingKey {
-        case food
-    }*/
-    
     var food: IngredientItem
     var id: Int { return UUID().hashValue }
     
@@ -38,84 +34,22 @@ class IngredientModel: Codable, Identifiable {
         food = f
     }
     
-   /* required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        food = try container.decode(IngredientItem.self, forKey: .food)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(food, forKey: .food)
-    }*/
 }
 
 
 class IngredientItem: Codable, Identifiable {
     
-   /* enum CodingKeys: CodingKey {
-        case foodId
-        case label
-        case knownAs
-    }*/
-    
     var foodId: String
     var label: String
     var knownAs: String
     var id: Int { return UUID().hashValue }
-
+    
     init(f: String = "", l: String = "", k: String = ""){
         foodId = f
         label = l
         knownAs = k
     }
     
-    /*required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        foodId = try container.decode(String.self, forKey: .foodId)
-        label = try container.decode(String.self, forKey: .label)
-        knownAs = try container.decode(String.self, forKey: .knownAs)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(foodId, forKey: .foodId)
-        try container.encode(label, forKey: .label)
-        try container.encode(knownAs, forKey: .knownAs)
-    }*/
 }
-
-
-/*@Model
-class NutrientsItem: Codable {
-    
-    enum CodingKeys: CodingKey {
-        case food
-    }
-    
-    var ENERC_KCAL: Double
-    var PROCNT: Double
-    var FAT: Double
-    var CHOCDF: Double
-    var FIBTG: Double
-    
-    init(e: Double = 0.0, p: Double = 0.0, fa: Double = 0.0, c: Double = 0.0, fi: Double = 0.0){
-        ENERC_KCAL = e
-        PROCNT = p
-        FAT = fa
-        CHOCDF = c
-        FIBTG = fi
-        
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        food = try container.decode(IngredientItem.self, forKey: .food)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(food, forKey: .food)
-    }
-}
-*/
+  
 
--- FinalProject/Pantry/View/InPantryView.swift
+++ FinalProject/Pantry/View/InPantryView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import SwiftData
-import Algorithms
 
 struct InPantryView: View {
     @Environment(\.modelContext) var modelContext
